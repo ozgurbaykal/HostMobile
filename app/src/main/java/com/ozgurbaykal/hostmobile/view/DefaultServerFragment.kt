96eb63a58518b6d752bf61ea4ad14cb7b07007f2
Özgür Baykal <ozgurbaykal@smartcozum.com>
2023-06-14 21:24:58 +0300

    add two fragment in mainActivity

---
+++ app/src/main/java/com/ozgurbaykal/hostmobile/view/DefaultServerFragment.kt
@@ -0,0 +1,48 @@
+package com.ozgurbaykal.hostmobile.view
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.RelativeLayout
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import com.ozgurbaykal.hostmobile.R
+import com.ozgurbaykal.hostmobile.databinding.FragmentDefaultServerBinding
+
+class DefaultServerFragment : Fragment(R.layout.fragment_default_server) {
+
+    private val TAG = "_DefaultServerFragment"
+
+    private var _binding: FragmentDefaultServerBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var advancedSettingsButton : RelativeLayout
+    private lateinit var dropDownLinear : LinearLayout
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        _binding = FragmentDefaultServerBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        advancedSettingsButton = binding.advancedSettingsRelativeLayoutButton
+        dropDownLinear = binding.dropDownLinear
+
+        advancedSettingsButton.setOnClickListener {
+            Log.i(TAG, " Advanced Settings Clicked")
+
+            if(dropDownLinear.isVisible)
+                dropDownLinear.visibility = View.GONE
+            else
+                dropDownLinear.visibility = View.VISIBLE
+        }
+
+        return view
+    }
+}
--- app/src/main/java/com/ozgurbaykal/hostmobile/view/MainActivity.kt
+++ app/src/main/java/com/ozgurbaykal/hostmobile/view/MainActivity.kt
@@ -3,13 +3,26 @@ package com.ozgurbaykal.hostmobile.view
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.ozgurbaykal.hostmobile.R
 import com.ozgurbaykal.hostmobile.control.SharedPreferenceManager
-
+import com.ozgurbaykal.hostmobile.databinding.ActivityMainBinding
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    private val TAG = "_MainActivity"
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var bottomNav : BottomNavigationView
+        override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
 
         SharedPreferenceManager.init(this@MainActivity)
 
@@ -19,5 +32,45 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        bottomNav = binding.bottomNavigation
+
+        bottomNav.setOnItemSelectedListener  {item ->
+            when (item.itemId) {
+                R.id.page_1_bottom_nav_button -> {
+                    Log.i(TAG, "Bottom Nav ClickEvent -> PAGE 1")
+                    changeFragment(CustomServerFragment(), R.id.main_fragment_view)
+                    return@setOnItemSelectedListener true
+                }
+                R.id.page_2_bottom_nav_button -> {
+                    Log.i(TAG, "Bottom Nav ClickEvent -> PAGE 2")
+                    changeFragment(DefaultServerFragment(), R.id.main_fragment_view)
+                    return@setOnItemSelectedListener true
+                }
+                R.id.page_3_bottom_nav_button -> {
+                    Log.i(TAG, "Bottom Nav ClickEvent -> PAGE 3")
+                    return@setOnItemSelectedListener true
+                }
+                R.id.page_4_bottom_nav_button -> {
+                    Log.i(TAG, "Bottom Nav ClickEvent -> PAGE 4")
+                    return@setOnItemSelectedListener true
+                }
+            }
+            false
+        }
+            if (savedInstanceState == null) {
+                supportFragmentManager.commit {
+                    setReorderingAllowed(true)
+                    add<CustomServerFragment>(R.id.main_fragment_view)
+                }
+            }
+
+        }
+
+    private fun changeFragment(fragment: Fragment, frameId: Int) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(frameId, fragment)
+            commit()
+        }
     }
+
 }
